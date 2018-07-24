8cd94d566ab4597624b85e90bca32c48aeec730b
jilucev <jilucev7@gmail.com>
2018-07-24 12:03:44 -0700

    Adds declare winning power to MagicalPower

--- app/models/magical_power.rb
+++ app/models/magical_power.rb
@@ -1,2 +1,7 @@
 class MagicalPower < ActiveRecord::Base
+
+  def declare_winning_power(power_one, power_two)
+    return 'tie' if power_one.intensity == power_two.intesnsity
+    power_one.intensity > power_two.intensity ? power_one : power_two
+  end
 end
