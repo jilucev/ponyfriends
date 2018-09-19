689d84c392d5235494f5e6ef02443ff64cb56f5c
jilucev <jilucev7@gmail.com>
2018-09-19 10:49:06 -0700

    lilac

--- app/models/pony.rb
+++ app/models/pony.rb
@@ -1,5 +1,5 @@
 class Pony < ActiveRecord::Base
-  # lavender pony rose ylang ylang jasmine hyancinth lilac tiger lily buttercup tomatoes satsuma
+  # lavender pony rose ylang ylang jasmine hyancinth lilac tiger lily buttercup tomatoes satsuma lilac
   has_many :magical_powers
   has_and_belongs_to_many :friends, through: :friends_ponies
 end
